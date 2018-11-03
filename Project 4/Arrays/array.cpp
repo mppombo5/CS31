2bae2efc4008edb89a53ad1992823c8abf15da88
mppombo5 <mppombo5@gmail.com>
2018-11-02 17:16:04 -0700

    appendToAll and lookup functions

--- Project 4/Arrays/array.cpp
+++ Project 4/Arrays/array.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+int appendToAll(string a[], int n, string value);
+int lookup(const string a[], int n, string target);
 
 int main() {
 
-    cout << "Hello, World!" << endl;
+    string arr[9] = {
+            "hey there", "hi", "hello", "howdy", "hola", "bonjour", "asdf", "bcde", "thicc thighs"
+    };
+
+    string s;
+    cout << "What string would you like to look for? ";
+    getline(cin, s);
+
+    int n;
+    cout << "Up to which position would you like to check? ";
+    cin >> n;
+
+    int pos = lookup(arr, n, s);
+
+    cout << '"' << s << '"' << " is in position " << pos << ", starting from 0." << endl;
 
     return 0;
+}
+
+int appendToAll(string a[], int n, string value) {
+
+    if (n < 0)
+        return -1;
+
+    for (int i = 0; i < n; i++) {
+        a[i] += value;
+    }
+
+    return n;
+}
+
+int lookup(const string a[], int n, string target) {
+
+    if (n < 0)
+        return -1;
+
+    for (int i = 0; i <= n; i++) {
+        if (a[i] == target) {
+            return i;
+        }
+    }
+    return -1;
 }
