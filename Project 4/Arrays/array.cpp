129d4b5985fd3c657556c0e6ec0a863a9cf70bbd
mppombo5 <mppombo5@gmail.com>
2018-11-06 20:58:03 -0800

    Finished all functions, probably

--- Project 4/Arrays/array.cpp
+++ Project 4/Arrays/array.cpp
@@ -17,45 +17,62 @@ int divide(string a[], int n, string divider);
 int main() {
 
     string h[7] = { "greg", "gavin", "ed", "xavier", "", "eleni", "fiona" };
+    string h1[7] = { "greg", "gavin", "ed", "fiona", "", "eleni", "xavier" };
     assert(lookup(h, 7, "eleni") == 5);
     assert(lookup(h, 7, "ed") == 2);
     assert(lookup(h, 2, "ed") == -1);
     assert(positionOfMax(h, 7) == 3);
 
+    // mine
+    assert(lookup(h, 7, "fiona") == 6);
+    assert(positionOfMax(h1, 7) == 6);
+
     string g[4] = { "greg", "gavin", "fiona", "kevin" };
     assert(differ(h, 4, g, 4) == 2);
+
+    //mine
+    assert(differ(h, 7, h1, 7) == 3);
+
     assert(appendToAll(g, 4, "?") == 4 && g[0] == "greg?" && g[3] == "kevin?");
     assert(rotateLeft(g, 4, 1) == 1 && g[1] == "fiona?" && g[3] == "gavin?");
 
-    string e[4] = { "ed", "xavier", "", "eleni" };
-    assert(subsequence(h, 7, e, 4) == 2);
+    string e[4] = { "ed", "fiona", "", "xavier" };
+    string e1[4] = { "fiona", "", "eleni", "xavier" };
+    assert(subsequence(h, 7, e, 4) == -1);
 
-    string d[5] = { "gavin", "gavin", "gavin", "xavier", "xavier" };
-    assert(countRuns(d, 5) == 2);
+    //mine
+    assert(subsequence(h1, 7, e1, 4) == 3);
+
+    string d[5] = { "gavin", "gavin", "gavin", "xavier", "eleni" };
+    string d1[5] = { "gavin", "gavin", "gavin", "xavier", "xavier" };
+    assert(countRuns(d, 5) == 3);
+
+    //mine
+    assert(countRuns(d1, 5) == 2);
 
     string f[3] = { "fiona", "ed", "john" };
     assert(lookupAny(h, 7, f, 3) == 2);
     assert(flip(f, 3) == 3 && f[0] == "john" && f[2] == "fiona");
 
-    // assert(divide(h, 7, "fiona") == 3);
+    assert(divide(h, 7, "fiona") == 3);
 
     cout << "All tests succeeded" << endl;
 
-    /*string arr[9] = {
+    string arrO[9] = {
+            "ape", "bonjour", "crapple", "denim", "effervescent", "forgery", "georgia", "heist", "icarus"
+    };
+
+    string arr[9] = {
             "ape", "bonjour", "crapple", "heist", "georgia", "forgery", "effervescent", "denim", "icarus"
     };
 
     string arr2[9] = {
-            "ape", "ape", "bonjour", "crapple", "crapple", "crapple", "heist", "georgia", "icarus"
+            "forgery", "georgia", "heist", "icarus", "crapple", "ape", "bonjour", "denim", "effervescent"
     };
 
-    int n;
-    cout << "Up to which position would you like to check for runs? ";
-    cin >> n;
+    assert(divide(arr2, 9, "z") == 9);
 
-    int p = countRuns(arr2, n);
-
-    cout << "countRuns returns " << p << ".\n";*/
+    cout << "All test cases succeeded!\n";
 
     return 0;
 }
@@ -77,7 +94,7 @@ int lookup(const string a[], int n, string target) {
     if (n < 0)
         return -1;
 
-    for (int i = 0; i < (n - 1); i++) {
+    for (int i = 0; i < n; i++) {
         if (a[i] == target) {
             return i;
         }
@@ -179,6 +196,10 @@ int subsequence(const string a1[], int n1, const string a2[], int n2) {
     if (n1 < 0 || n2 < 0 || n2 > n1)
         return -1;
 
+    if (n2 == 0)
+        return -1;
+
+    // i is the position of the string in a1, j is the position in a2
     int i = 0;
     int j = 0;
     while (i < n1) {
@@ -217,5 +238,46 @@ int divide(string a[], int n, string divider) {
     if (n < 0)
         return -1;
 
+    int i = 0;
+    int j = (n - 1);
 
+    // only go until they reach each other
+    while (i <= j) {
+        // increment i until a[i] ≥ divider
+        while (a[i] < divider) {
+            i++;
+        }
+        // decrement j until a[j] < divider
+        while (a[j] >= divider)
+            j--;
+        // swap the two values only if i is still less than j
+        if (i <= j) {
+            string latter = a[j];
+            a[j] = a[i];
+            a[i] = latter;
+            i++;
+            j--;
+        }
+    }
+
+    // dividerPos is the position of divider in the array, -1 if it's not in the array
+    int dividerPos = -1;
+    for (int k = 0; k < n; k++) {
+        if (a[k] == divider) {
+            dividerPos = k;
+            break;
+        }
+    }
+
+    if (dividerPos != -1) {
+        string tmp = a[dividerPos];
+        a[dividerPos] = a[i];
+        a[i] = tmp;
+    }
+
+    for (int k = 0; k < n; k++) {
+        if (a[k] >= divider)
+            return k;
+    }
+    return n;
 }
