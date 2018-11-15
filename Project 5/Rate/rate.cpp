7372c569dc30bc03efdf0287ba515ba803721553
mppombo5 <mppombo5@gmail.com>
2018-11-15 02:35:03 -0800

    Finished makeProper, started rate

--- Project 5/Rate/rate.cpp
+++ Project 5/Rate/rate.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <cassert>
+#include <cctype>
 using namespace std;
 
 const int MAX_WORD_LENGTH = 20;
 
-void makeUppercase(char words[][MAX_WORD_LENGTH+1], int n);
+const int MAX_DOC_LENGTH = 250;
+
+void makeUppercase2D(char words[][MAX_WORD_LENGTH+1], int n);
 
 void rotateLeftAll(char a1[][MAX_WORD_LENGTH+1], char a2[][MAX_WORD_LENGTH+1], int sep[], int n, int pos);
+
+void rotateLeftOne(char a[], int n, int pos);
 
 int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1],
                int separation[], int nPatterns);
@@ -20,51 +25,27 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
 
 int main() {
 
-    char test1[6][MAX_WORD_LENGTH+1] = {
-            {"HELLO"},
-            {"THERE"},
-            {"GENERAL"},
-            {"KENOBI"},
-            {"YOU"},
-            {"ARE"}
+    const int TEST1_NRULES = 4;
+
+    char doc[MAX_DOC_LENGTH] = "  That     plot: NEFARIOUS!";
+
+    char test1w1[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
+            "mad",       "deranged", "nefarious", "have"
     };
-    char test2[6][MAX_WORD_LENGTH+1] = {
-            {"A"},
-            {"BOLD"},
-            {"ONE"},
-            {"YOURE"},
-            {"SHORTER"},
-            {"THAN"}
+    char test1w2[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
+            "scientist", "robot",    "plot",      "mad"
     };
-    int testSep[6] = {
-            1, 2, 3, 4, 5, 6
+    int test1dist[TEST1_NRULES] = {
+            1,           3,          0,           12
     };
 
-    int m = makeProper(test1, test2, testSep, 6);
-
-    for (int i = 0; i < m; i++) {
-        for (int k = 0; test1[i][k] != '\0'; k++) {
-            cout << test1[i][k];
-        }
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < m; i++) {
-        for (int k = 0; test2[i][k] != '\0'; k++) {
-            cout << test2[i][k];
-        }
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < m; i++) {
-        cout << testSep[i];
-        cout << endl;
-    }
+    int m = rate(doc, test1w1, test1w2, test1dist, TEST1_NRULES);
 
     return 0;
 }
 
-void makeUppercase(char words[][MAX_WORD_LENGTH+1], int n) {
+void makeUppercase2D(char words[][MAX_WORD_LENGTH+1], int n) {
+
     for (int i = 0; i < n; i++) {
         for (int k = 0; words[i][k] != '\0'; k++) {
             words[i][k] = (char) tolower(words[i][k]);
@@ -72,14 +53,30 @@ void makeUppercase(char words[][MAX_WORD_LENGTH+1], int n) {
     }
 }
 
+void makeUppercase1D(char a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        a[i] = (char) tolower(a[i]);
+    }
+}
+
 // use this function to remove the element at position pos and shift all other elements left
 // pretty similar to rotateLeft from project 4
+// to use in makeProper: word1, word2, separation to move those; nPatternsNew shift until nPatternsNew;
+// last argument should be the position of the element you want to get rid of
 void rotateLeftAll(char a1[][MAX_WORD_LENGTH+1], char a2[][MAX_WORD_LENGTH+1], int sep[], int n, int pos) {
 
     for (int i = pos; i < (n-1); i++) {
         strcpy(a1[i], a1[i+1]);
         strcpy(a2[i], a2[i+1]);
         sep[i] = sep[i+1];
+    }
+}
+
+void rotateLeftOne(char a[], int n, int pos) {
+
+    for (int i = pos; i < (n-1); i++) {
+        a[i] = a[i+1];
     }
 }
 
@@ -93,54 +90,102 @@ int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1],
     if (nPatterns <= 0)
         return 0;
 
-    // make new number r which starts as nPatterns, decrease it as you remove patterns
-    int r = nPatterns;
+    // make new number nPatternsNew which starts as nPatterns, decrease it as you remove patterns
+    int nPatternsNew = nPatterns;
 
     // make both string arrays uppercase to match requirements
-    makeUppercase(word1, nPatterns);
-    makeUppercase(word2, nPatterns);
+    makeUppercase2D(word1, nPatterns);
+    makeUppercase2D(word2, nPatterns);
 
-    // check up to r, because the length of the array could change
-    for (int i = 0; i < r; i++) {
+    // make this boolean to see if the element has already been removed
+    bool removed;
+
+    // check up to nPatternsNew, because the length of the array could change
+    for (int i = 0; i < nPatternsNew; i++) {
+        removed = false;
         // first, remove all elements in the arrays for which the separation value is negative
         if (separation[i] < 0) {
-            // move all elements in those three arrays left at position i all the way to r
-            rotateLeftAll(word1, word2, separation, r, i);
+            // move all elements in those three arrays left at position i all the way to nPatternsNew
+            rotateLeftAll(word1, word2, separation, nPatternsNew, i);
 
             // decrease new number of patterns by 1
-            r--;
+            nPatternsNew--;
 
             // decrease i so that it goes back over the same position, which now contains the old proceeding value
             // could probably just use a while loop but i don't feel like it
             i--;
+            removed = true;
         }
+        if (removed)
+            continue;
 
         // next, go through each string and check if they contain anything that's not a letter
         for (int k = 0; word1[i][k] != '\0'; k++) {
             if (!isalpha(word1[i][k])) {
-                rotateLeftAll(word1, word2, separation, r, i);
-                r--;
+                rotateLeftAll(word1, word2, separation, nPatternsNew, i);
+                nPatternsNew--;
                 i--;
+                removed = true;
+                break;
             }
         }
+        if (removed)
+            continue;
         //literally the same thing but for the second string array
         for (int k = 0; word2[i][k] != '\0'; k++) {
             if (!isalpha(word2[i][k])) {
-                rotateLeftAll(word1, word2, separation, r, i);
-                r--;
+                rotateLeftAll(word1, word2, separation, nPatternsNew, i);
+                nPatternsNew--;
                 i--;
+                removed = true;
+                break;
             }
         }
+        if (removed)
+            continue;
 
-        // now remove any element positions where there are no characters ('')
+        // next, remove any element positions where there are no characters ('')
         if (strcmp(word1[i], "") == 0 || strcmp(word2[i], "") == 0) {
-            rotateLeftAll(word1, word2, separation, r, i);
-            r--;
+            rotateLeftAll(word1, word2, separation, nPatternsNew, i);
+            nPatternsNew--;
             i--;
+            removed = true;
         }
+        if (removed)
+            continue;
 
+        // lastly, check for duplicates in either order
+        // this one will probably be a bit more involved
     }
-    return r;
+
+    // lastly, check for duplicates in either order
+    // this one will probably be a bit more involved
+    char tempWord1[MAX_WORD_LENGTH+1];
+    char tempWord2[MAX_WORD_LENGTH+1];
+    for (int i = 0; i < nPatternsNew; i++) {
+        // create temporary string at word[i] for each iteration of the for loop
+        strcpy(tempWord1, word1[i]);
+        strcpy(tempWord2, word2[i]);
+
+        for (int k = 0; k < nPatternsNew; k++) {
+            // there are only 4 combinations of strings that would be equal; check for those,
+            // rotate and prep for next if you have a match (duplicate)
+            if ( (k != i)
+             && ((strcmp(tempWord1, word1[k]) == 0 && strcmp(tempWord2, word2[k]) == 0)
+             ||  (strcmp(tempWord1, word2[k]) == 0 && strcmp(tempWord2, word1[k]) == 0))) {
+                if (separation[i] <= separation[k]) {
+                    rotateLeftAll(word1, word2, separation, nPatternsNew, i);
+                }
+                else {
+                    rotateLeftAll(word1, word2, separation, nPatternsNew, k);
+                }
+                nPatternsNew--;
+                k--;
+                continue;
+            }
+        }
+    }
+    return nPatternsNew;
 }
 
 // here all you do is count how many times the words with 'separation' between them are found
@@ -148,5 +193,29 @@ int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1],
 int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
          const char word2[][MAX_WORD_LENGTH+1], const int separation[], int nPatterns) {
 
-    return 0;
+    // create a copy of 'document' you can work with (make lowercase, make into proper form etc)
+    char doc[MAX_DOC_LENGTH];
+    strcpy(doc, document);
+
+    makeUppercase1D(doc, MAX_DOC_LENGTH);
+
+    // get rid of any characters that aren't letters or spaces
+    for (int i = 0; doc[i] != '\0'; i++) {
+        if (doc[i] != ' ' && !isalpha(doc[i])) {
+            rotateLeftOne(doc, MAX_DOC_LENGTH, i);
+            i--;
+        }
+    }
+
+    // this makes it so any spaces are limited to only one – might help when checking?
+    for (int i = 1; doc[i] != '\0'; i++) {
+        if (doc[i] == ' ' && doc[i-1] == ' ') {
+            rotateLeftOne(doc, MAX_DOC_LENGTH, i);
+            i--;
+        }
+    }
+
+    for (int i = 0; doc[i] != '\0'; i++) {
+        cerr << doc[i];
+    }
 }
