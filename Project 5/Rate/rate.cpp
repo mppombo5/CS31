7549967b9aa99015b7cb568f850b7a2a2ccc6f30
mppombo5 <mppombo5@gmail.com>
2018-11-18 16:13:28 -0800

    First working version of rate function

--- Project 5/Rate/rate.cpp
+++ Project 5/Rate/rate.cpp
@@ -26,9 +26,6 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
 int main() {
 
     const int TEST1_NRULES = 4;
-
-    char doc[MAX_DOC_LENGTH] = "The mad UCLA scientist unleashed a deranged evil giant robot.";
-
     char test1w1[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
             "mad",       "deranged", "nefarious", "have"
     };
@@ -38,8 +35,19 @@ int main() {
     int test1dist[TEST1_NRULES] = {
             1,           3,          0,           12
     };
-
-    int m = rate(doc, test1w1, test1w2, test1dist, TEST1_NRULES);
+    assert(rate("The mad UCLA scientist unleashed a deranged evil giant robot.",
+                test1w1, test1w2, test1dist, TEST1_NRULES) == 2);
+    assert(rate("The mad UCLA scientist unleashed    a deranged robot.",
+                test1w1, test1w2, test1dist, TEST1_NRULES) == 2);
+    assert(rate("**** 2018 ****",
+                test1w1, test1w2, test1dist, TEST1_NRULES) == 0);
+    assert(rate("  That plot: NEFARIOUS!",
+                test1w1, test1w2, test1dist, TEST1_NRULES) == 1);
+    assert(rate("deranged deranged robot deranged robot robot",
+                test1w1, test1w2, test1dist, TEST1_NRULES) == 1);
+    //assert(rate("That scientist said two mad scientists suffer from deranged-robot fever.",
+                   //test1w1, test1w2, test1dist, TEST1_NRULES) == 0);
+    cout << "All tests succeeded" << endl;
 
     return 0;
 }
@@ -183,8 +191,6 @@ int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1],
     return nPatternsNew;
 }
 
-// here all you do is count how many times the words with 'separation' between them are found
-// nowhere do you have to make sure the words are 'separation' apart
 int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
          const char word2[][MAX_WORD_LENGTH+1], const int separation[], int nPatterns) {
 
@@ -193,6 +199,11 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
 
     // create a copy of 'document' you can work with (make lowercase, make into proper form etc)
     char doc[MAX_DOC_LENGTH];
+    // this might seem redundant, but when assert cases are run one after the other
+    // it's not cleared, so there's garbage left over
+    for (int i = 0; i < MAX_DOC_LENGTH; i++) {
+        doc[i] = '\0';
+    }
     strcpy(doc, document);
 
     makeUppercase1D(doc, MAX_DOC_LENGTH);
@@ -216,44 +227,87 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
         rotateLeftOne(doc, MAX_DOC_LENGTH, 0);
     }
 
-    // at this point, we can start checking for rates and such now that our document is in a palatable form
+    // put all the words from document[] into a new array, each as an element
+    // make it more palatable than one long C string
 
-    // create new 2D array to put created strings into
-    char doc1[MAX_DOC_LENGTH][MAX_DOC_LENGTH];
+    // create new 2D array to put created strings into. Think of this as doc converted into an array
+    // of C-strings.
+    // side note: I think MAX_DOC_LENGTH is a reasonable bound to put on the maximum size of a word,
+    // since there apparently is no limit on the word size in document
+    char docAsArray[MAX_DOC_LENGTH][MAX_DOC_LENGTH];
+    for (int i = 0; i < MAX_DOC_LENGTH; i++) {
+        strcpy(docAsArray[i], "");
+    }
 
     // index at the start to increment through doc
-    int i = 0;
+    int index = 0;
+
     // first position of the string
     int charStart = 0;
+
     // counter for words in 2D array
     int wordCounter = 0;
-    while (i <= MAX_DOC_LENGTH && doc[i] != '\0') {
+
+    // this is where all the words are put into docAsArray
+    while (index <= MAX_DOC_LENGTH && doc[index] != '\0') {
         // temporary word to add to new string array
         char tempWord[MAX_DOC_LENGTH] = "";
-        // make sure to check for the zero byte, otherwise it'll keep checking until it hits who knows what space and where
-        while (doc[i] != ' ' && doc[i] != '\0') {
-            i++;
+        // make sure to check for the zero byte, otherwise it'll keep checking until it hits
+        // who knows what space and where in memory
+        while (doc[index] != ' ' && doc[index] != '\0') {
+            index++;
         }
         int j = 0;
-        for (int k = charStart; k < i; k++) {
+        for (int k = charStart; k < index; k++) {
             tempWord[j] = doc[k];
             j++;
         }
+        // stick a zero byte on the end to finish the string
         tempWord[j+1] = '\0';
-        // debug statement to make sure it's outputting the right words
-        /*for (int n = 0; tempWord[n] != '\0'; n++) {
-         * cerr << tempWord[n];
-        }*/
-        strcpy(doc1[wordCounter], tempWord);
-        i++;
-        charStart = i;
+        // move tempWord into the corresponding space in docAsArray
+        strcpy(docAsArray[wordCounter], tempWord);
+        index++;
+        charStart = index;
         wordCounter++;
     }
-    for (int d = 0; d < wordCounter; d++) {
-        cerr << doc1[d] << endl;
+
+    // docAsArray now has all the words in document, so now we can start counting the rates
+
+    int rateCounter = 0;
+    // outer for loop to check word1/word2
+    for (int i = 0; i < nPatterns; i++) {
+        bool foundMatch = false;
+        // another loop to compare each word in docAsArray to word1[i]
+        for (int k = 0; k < wordCounter; k++) {
+            if (foundMatch)
+                break;
+            if (strcmp(docAsArray[k], word1[i]) == 0) {
+                // startIndex to determine where to start the check for matching words – make 0 if
+                // the current position in docAsArray minus separation of that value is less than 0
+                int startIndex;
+                // compare separation[i] to k, as that'll give whether or not it's within bounds of docAsArray
+                if ((k - (separation[i] + 1)) < 0)
+                    startIndex = 0;
+                else
+                    startIndex = i - (separation[i] + 1);
+                // start index at startIndex, which will be the right position; increment until either j reaches
+                // i + separation[i], or until j reaches the end of docAsArray
+                for (int j = startIndex; j < (k + (separation[i] + 1)) || j < wordCounter; j++) {
+                    if (strcmp(docAsArray[j], word2[i]) == 0) {
+                        rateCounter++;
+                        foundMatch = true;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
-    // experiment for converting to strings ends here ===========================================================
+    // debug statement to check that elements of docAsArray are correct
+    /*for (int d = 0; d < wordCounter; d++) {
+        cerr << docAsArray[d] << endl;
+    }
+    cerr << "This array contains " << wordCounter << " words.\n";*/
 
-    return 0;
+    return rateCounter;
 }
