07ae8b40b6a12f80d671884c2ca501231126b8b6
mppombo5 <mppombo5@gmail.com>
2018-12-05 23:40:24 -0800

    Only commit for Project 7, forgot to commit at the start

--- Project 5/Rate/rate.cpp
+++ Project 5/Rate/rate.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <cassert>
 #include <cctype>
+#include <cstdlib>
 using namespace std;
 
 const int MAX_WORD_LENGTH = 20;
@@ -23,7 +24,7 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
 // nPatterns is the number of pairs of words in the arrays, could consider it the number in the first bracket
 // therefore, nPatterns is the position of both words AND the separation value
 
-int main() {
+int testingMain() {
 
     const int TEST1_NRULES = 4;
     char test1w1[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
@@ -296,4 +297,345 @@ int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1],
     cerr << "This array contains " << wordCounter << " words.\n";*/
 
     return rateCounter;
+}
+
+int d[3] =                      {    1,    0,    2 };
+char w1[3][MAX_WORD_LENGTH+1] = { "aa", "bb", "cc" };
+char w2[3][MAX_WORD_LENGTH+1] = { "dd", "ee", "ff" };
+
+int e[2] =                      {    3,    3 };
+char x1[2][MAX_WORD_LENGTH+1] = { "aa", "cc" };
+char x2[2][MAX_WORD_LENGTH+1] = { "bb", "dd" };
+
+const int THREE_LETTERS = 26*26*26;
+int dd[THREE_LETTERS];
+char ww1[THREE_LETTERS][MAX_WORD_LENGTH+1];
+char ww2[THREE_LETTERS][MAX_WORD_LENGTH+1];
+
+bool eq(int c, int dist, const char wd1[], const char wd2[])
+{
+    return d[c] == dist  &&  strcmp(w1[c], wd1) == 0  &&
+           strcmp(w2[c], wd2) == 0;
+}
+
+void testone(int n)
+{
+    switch (n)
+    {
+        case  1: {
+            assert(makeProper(w1, w2, d, 0) == 0);
+        } break; case  2: {
+            assert(makeProper(w1, w2, d, 1) == 1  &&
+                   eq(0, 1, "aa", "dd"));
+        } break; case  3: {
+            d[0] = -1;
+            assert(makeProper(w1, w2, d, 1) == 0);
+        } break; case  4: {
+            d[0] = -1;
+            assert(makeProper(w1, w2, d, 2) == 1  &&
+                   eq(0, 0, "bb", "ee"));
+        } break; case  5: {
+            d[1] = -1;
+            assert(makeProper(w1, w2, d, 2) == 1  &&
+                   eq(0, 1, "aa", "dd"));
+        } break; case  6: {
+            d[1] = -1;
+            d[2] = -1;
+            assert(makeProper(w1, w2, d, 3) == 1  &&
+                   eq(0, 1, "aa", "dd"));
+        } break; case  7: {
+            d[0] = -1;
+            d[1] = -1;
+            assert(makeProper(w1, w2, d, 3) == 1  &&
+                   eq(0, 2, "cc", "ff"));
+        } break; case  8: {
+            d[0] = -1;
+            d[1] = -1;
+            d[2] = -1;
+            assert(makeProper(w1, w2, d, 3) == 0);
+        } break; case  9: {
+            strcpy(w1[1], "");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 10: {
+            strcpy(w2[1], "");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 11: {
+            strcpy(w1[1], "bb@");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 12: {
+            strcpy(w1[1], "bb ");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 13: {
+            strcpy(w1[1], "bBBb");
+            assert(makeProper(w1, w2, d, 2) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 0, "bbbb", "ee"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 0, "bbbb", "ee"))
+                   )
+            );
+        } break; case 14: {
+            strcpy(w1[1], "bBBb");
+            strcpy(w2[1], "EeEeE");
+            assert(makeProper(w1, w2, d, 2) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 0, "bbbb", "eeeee"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 0, "bbbb", "eeeee"))
+                   )
+            );
+        } break; case 15: {
+            strcpy(w2[1], "bb");
+            assert(makeProper(w1, w2, d, 2) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 0, "bb", "bb"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 0, "bb", "bb"))
+                   )
+            );
+        } break; case 16: {
+            strcpy(w2[0], "aa");
+            strcpy(w1[1], "aa");
+            strcpy(w2[1], "aa");
+            assert(makeProper(w1, w2, d, 2) == 1  &&
+                   eq(0, 1, "aa", "aa")
+            );
+        } break; case 17: {
+            strcpy(w1[1], "aa");
+            strcpy(w2[1], "dd");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 18: {
+            strcpy(w1[1], "aa");
+            strcpy(w2[1], "dd");
+            d[1] = 1;
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 19: {
+            strcpy(w1[1], "aa");
+            strcpy(w2[1], "dd");
+            d[1] = 4;
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 4, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 4, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 20: {
+            strcpy(w1[1], "aa");
+            strcpy(w2[1], "dd");
+            strcpy(w1[2], "aa");
+            strcpy(w2[2], "dd");
+            assert(makeProper(w1, w2, d, 3) == 1  &&
+                   eq(0, 2, "aa", "dd")
+            );
+        } break; case 21: {
+            strcpy(w1[2], "aa");
+            strcpy(w2[2], "dd");
+            d[1] = -1;
+            assert(makeProper(w1, w2, d, 3) == 1  &&
+                   eq(0, 2, "aa", "dd")
+            );
+        } break; case 22: {
+            strcpy(w1[1], "abcdefghijklmnopqrst");
+            strcpy(w2[1], "abcdefghijklmnopqrsu");
+            d[0] = -1;
+            assert(makeProper(w1, w2, d, 2) == 1  &&
+                   eq(0, 0, "abcdefghijklmnopqrst",
+                      "abcdefghijklmnopqrsu")
+            );
+        } break; case 23: {
+            strcpy(w2[0], "DD");
+            strcpy(w1[1], "AA");
+            strcpy(w2[1], "dD");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 24: {
+            strcpy(w1[1], "dd");
+            strcpy(w2[1], "aa");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 25: {
+            strcpy(w1[1], "dd");
+            strcpy(w2[1], "aa");
+            d[1] = 1;
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   (((eq(0, 1, "aa", "dd")  ||  eq(0, 1, "dd", "aa")
+                     )  &&  eq(1, 2, "cc", "ff")
+                    )  ||
+                    ((eq(1, 1, "aa", "dd")  ||  eq(1, 1, "dd", "aa")
+                     )  &&  eq(0, 2, "cc", "ff")
+                    )
+                   )
+            );
+        } break; case 26: {
+            strcpy(w1[1], "Dd");
+            strcpy(w2[1], "aA");
+            assert(makeProper(w1, w2, d, 3) == 2  &&
+                   ((eq(0, 1, "aa", "dd")  &&  eq(1, 2, "cc", "ff"))  ||
+                    (eq(1, 1, "aa", "dd")  &&  eq(0, 2, "cc", "ff"))
+                   )
+            );
+        } break; case 27: {
+            int dx[7] = { 1, 3, 0, 2, 1, 0, 12 };
+            char w1x[7][MAX_WORD_LENGTH+1] = { "mad", "deranged",
+                                               "NEFARIOUS", "half-witted", "robot", "plot", "have" };
+            char w2x[7][MAX_WORD_LENGTH+1] = { "scientist", "robot",
+                                               "PLOT", "assistant", "deranged", "Nefarious", "mad" };
+            assert(makeProper(w1x, w2x, dx, 7) == 4);
+        } break; case 28: {
+            assert(rate("", x1, x2, e, 2) == 0);
+        } break; case 29: {
+            assert(rate(" ", x1, x2, e, 2) == 0);
+        } break; case 30: {
+            assert(rate("aa bb", x1, x2, e, 2) == 1);
+        } break; case 31: {
+            e[0] = 0;
+            assert(rate("aa bb", x1, x2, e, 2) == 1);
+        } break; case 32: {
+            e[0] = 0;
+            assert(rate("aa     bb", x1, x2, e, 2) == 1);
+        } break; case 33: {
+            assert(rate(" aa bb", x1, x2, e, 2) == 1);
+        } break; case 34: {
+            assert(rate("aa bb ", x1, x2, e, 2) == 1);
+        } break; case 35: {
+            assert(rate("aa bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 36: {
+            assert(rate("aa bb cc dd xx", x1, x2, e, 1) == 1);
+        } break; case 37: {
+            assert(rate("aa xx cc bb xx dd xx", x1, x2, e, 2) == 2);
+        } break; case 38: {
+            assert(rate("xx dd bb cc aa xx", x1, x2, e, 2) == 2);
+        } break; case 39: {
+            assert(rate("aa xx xx xx bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 40: {
+            assert(rate("aa xx xx xx xx bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 41: {
+            e[0] = 0;
+            e[1] = 0;
+            assert(rate("aa xx bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 42: {
+            e[0] = 1;
+            e[1] = 1;
+            assert(rate("aa cc bb dd xx", x1, x2, e, 2) == 2);
+        } break; case 43: {
+            assert(rate("aa bb aa bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 44: {
+            assert(rate("aa aa bb bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 45: {
+            strcpy(x2[0], "aa");
+            assert(rate("aa xx xx aa", x1, x2, e, 2) == 1);
+        } break; case 46: {
+            strcpy(x2[0], "aa");
+            assert(rate("xx aa xx", x1, x2, e, 2) == 0);
+        } break; case 47: {
+            assert(rate("aA bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 48: {
+            assert(rate("aa @@ ## $$ %% ^^ bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 49: {
+            assert(rate("aa b@@b cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 50: {
+            assert(rate("aa b123456789012345678901234567890b cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 51: {
+            e[0] = 0;
+            assert(rate("aa abcdefghijklmnopqrstuvwxyz bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 52: {
+            e[0] = 1;
+            assert(rate("aa abcdefghijklmnopqrstuvwxyz bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 53: {
+            strcpy(x1[0], "abcdefghijklmnopqrst");
+            assert(rate("abcdefghijklmnopqrst bb cc dd xx", x1, x2, e, 2) == 2);
+        } break; case 54: {
+            strcpy(x1[0], "abcdefghijklmnopqrst");
+            assert(rate("abcdefghijklmnopqrstu bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 55: {
+            assert(rate("aaa bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 56: {
+            assert(rate("aa@bb cc dd xx", x1, x2, e, 2) == 1);
+        } break; case 57: {
+            assert(rate("aa bb@cc dd xx", x1, x2, e, 2) == 0);
+        } break; case 58: {
+            e[0] = 1000;
+            assert(rate("xx aa xx", x1, x2, e, 2) == 0);
+        } break; case 59: {
+            strcpy(x1[0], "a");
+            strcpy(x2[0], "b");
+            e[0] = 123;
+            char doc[250+1];
+            strcpy(doc, "a ");
+            for (int k = 0; k < 123; k++)
+                strcpy(doc+2+2*k, "x ");
+            strcpy(doc+248, "b ");
+            assert(rate(doc, x1, x2, e, 2) == 1);
+        } break; case 60: {
+            for (int k = 0; k < THREE_LETTERS; k++)
+            {
+                dd[k] = (k % 2) - 1;
+                strcpy(ww1[k], "a");
+                strcpy(ww2[k], "aaa");
+                int kk = k;
+                ww2[k][2] += kk % 26;
+                kk /= 26;
+                ww2[k][1] += kk % 26;
+                kk /= 26;
+                ww2[k][0] += kk;
+            }
+            assert(makeProper(ww1, ww2, dd, THREE_LETTERS) == THREE_LETTERS / 2);
+            for (int k = 0; k < THREE_LETTERS / 2; k++)
+                assert(dd[k] == 0 && strcmp(ww1[k], "a") == 0 &&
+                       islower(ww2[k][2]) &&
+                       (ww2[k][2] - 'a') % 2 == 1);
+        } break; case 61: {
+            for (int k = 0; k < THREE_LETTERS; k++)
+            {
+                dd[k] = 0;
+                strcpy(ww1[k], "a");
+                strcpy(ww2[k], "aaa");
+                int kk = k;
+                ww2[k][2] += kk % 26;
+                kk /= 26;
+                ww2[k][1] += kk % 26;
+                kk /= 26;
+                ww2[k][0] += kk;
+            }
+            assert(rate("a aaa a iaa a pzz a zzz", ww1, ww2, dd, THREE_LETTERS) == 4);
+            assert(rate("a aaaa a iaaa a pzzz a zzzz", ww1, ww2, dd, THREE_LETTERS) == 0);
+        } break;
+    }
+}
+
+int main()
+{
+    cout << "Enter a test number (1 to 61): ";
+    int n;
+    cin >> n;
+    if (n < 1  ||  n > 61)
+    {
+        cout << "Bad test number" << endl;
+        return 1;
+    }
+    testone(n);
+    cout << "Passed test " << n << endl;
 }
